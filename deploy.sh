6aa12f1e67dccce0e26cff6f574e6cbf049e1ba2
Urick Loureiro <uloureiro@enova.com>
2020-07-23 19:43:56 -0300

    add Travis autobuild

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,9 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:uloureiro/random-stickies.git master:gh-pages
