554eac8a6e93272b48b6eab89db38573bcb38930
Urick Loureiro <uloureiro@enova.com>
2020-07-23 21:06:54 -0300

    add git configuration to CircleCI

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,13 @@
 # abort on errors
 set -e
 
+git config --global -l
+git config --global user.email circleci@circleci
+git config --global user.name CircleCI
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "CircleCI Deploy (`date '+%F %T %Z'`)"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:uloureiro/random-stickies.git master:gh-pages
